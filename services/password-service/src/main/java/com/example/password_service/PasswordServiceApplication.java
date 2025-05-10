5679d56e4662293f700b576e0ce2cee2fef531b3
Phan Tien Tai <taip7547@gmail.com>
2025-05-10 21:52:20 +0700

    Add Password Service

---
+++ services/password-service/src/main/java/com/example/password_service/PasswordServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.password_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordServiceApplication.class, args);
+	}
+
+}
---
+++ services/password-service/src/test/java/com/example/password_service/PasswordServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.password_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
